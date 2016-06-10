cdd58e08d8bb63cfa1f0b4b3afa6ec25ebae2d31
angelamiller0120 <angelamiller0120@icloud.com>
2016-06-11 01:09:04 +0200

    0.0.1

--- 0610.py
+++ 0610.py
@@ -1,1 +1,9 @@
+import requests
+url = "http://opendata.paris.fr/explore/dataset/subventions-accordees-et-refusees/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true"
+r = requests.get(url)
+with open("data.csv","wb") as code:
+    code.write(r.content)
+
 import pandas as pd
+import numpy as np
+data=pd.read_csv("data.csv")
